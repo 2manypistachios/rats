a9683860a7b42c8862b16b6848bc83f2c928053c
Sumaxi <max@podolski.org>
2018-09-08 13:39:49 -0400

    Removed unnecessary widget

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,26 +4,3 @@ import "netlify-cms/dist/cms.css";
 
 //CMS.registerPreviewStyle('/styles.css')
 console.log("CMS", CMS);
-
-var CategoriesControl = createClass({
-    handleChange: function(e) {
-      this.props.onChange(e.target.value.split(',').map((e) => e.trim()));
-    },
-  
-    render: function() {
-      var value = this.props.value;
-      return h('input', { type: 'text', value: value ? value.join(', ') : '', onChange: this.handleChange });
-    }
-  });
-  
-  var CategoriesPreview = createClass({
-    render: function() {
-      return h('ul', {},
-        this.props.value.map(function(val, index) {
-          return h('li', {key: index}, val);
-        })
-      );
-    }
-  });
-  
-//CMS.registerWidget('categories', CategoriesControl, CategoriesPreview);
