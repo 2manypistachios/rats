294d27060c369412da5a50b45fcddfc854f87b35
Sumaxi <max@podolski.org>
2018-09-08 13:54:50 -0400

    Direct import css

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -1,6 +1,7 @@
 import React from "react";
 import CMS from "netlify-cms";
 import "netlify-cms/dist/cms.css";
+import "./cms.css";
 
 //CMS.registerPreviewStyle('/styles.css')
 console.log("CMS", CMS);
